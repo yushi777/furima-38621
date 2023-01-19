7cd424056f915bf7e75faa2dfdc81b3d1b989ed9
yushi777 <ucy77777@gmail.com>
2023-01-19 16:24:37 +0900

    usersテーブルの作成

--- db/migrate/20230119070625_devise_create_users.rb
+++ db/migrate/20230119070625_devise_create_users.rb
@@ -6,6 +6,13 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :nick_name,          null: false
+      t.string :first_name,         null: false
+      t.string :last_name,          null: false
+      t.string :read_first_name,    null: false
+      t.string :read_last_name,     null: false
+      t.date   :birth_day,          null: false
+
 
       ## Recoverable
       t.string   :reset_password_token
