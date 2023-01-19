201d52fcdcb6aaf1e07fee94430f1ec0abe9d24e
yushi777 <ucy77777@gmail.com>
2023-01-19 18:22:58 +0900

    user.rbにバリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         validates :nick_name, presence: true
+         validates :first_name, presence: true
+         validates :last_name, presence: true
+         validates :read_first_name, presence: true
+         validates :read_last_name, presence: true
+         validates :birth_day, presence: true
+
+         
 end
