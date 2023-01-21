872dd95a1cd4281e0ca111333b380d927f39eb08
yushi777 <ucy77777@gmail.com>
2023-01-21 09:52:30 +0900

    バリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
          validates :nick_name, presence: true
-         validates :first_name, presence: true
-         validates :last_name, presence: true
-         validates :read_first_name, presence: true
-         validates :read_last_name, presence: true
+         validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]/ }
+         validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]/ }
+         validates :read_first_name, presence: true, format: { with: /\A[ァ-ヶ一ー]+\z/ }
+         validates :read_last_name, presence: true, format: { with: /\A[ァ-ヶ一ー]+\z/ }
          validates :birth_day, presence: true
 
          
