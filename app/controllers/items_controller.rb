7bd720b4dfbda5dc60ea7eb166ed1b19fc9859a9
yushi777 <ucy77777@gmail.com>
2023-01-24 21:14:46 +0900

    ActiveStorageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,4 +3,10 @@ class ItemsController < ApplicationController
   end
   def new
   end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :product_name, :category_id, :price, :product_description, :product_condition_id, :shipping_charge_id, :prefecture_id, :delivery_day_id).merge(user_id: current_user.id)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,7 @@
 class Item < ApplicationRecord
+  belongs_to :user
+  has_one :order
+
+  has_one_attached :image
+
 end
