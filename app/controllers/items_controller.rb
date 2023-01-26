fa59e8f2c6c4d7edfed8863e679d73f3316ca538
yushi777 <ucy77777@gmail.com>
2023-01-26 11:56:55 +0900

    itemモデルにモジュールの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,22 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+
+
+
   def index
     @item = Item.includes(:user).order('created_at DESC')
   end
   def new
     @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,10 +1,14 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+
   belongs_to :user
   has_one :order
 
   belongs_to :category
   belongs_to :product_condition
   belongs_to :shipping_charge
+  belongs_to :prefecture
+  belongs_to :delivery_day
 
 
   has_one_attached :image
@@ -26,6 +30,7 @@ class Item < ApplicationRecord
   validates :shipping_charge_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :delivery_day_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :price, numericality: { other_than: 1 }
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
+                    format: { with: /\A[0-9]+\z/ }
 
 end
