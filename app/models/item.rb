94fede408157d73bd08039eef5ee95e803691b1c
yushi777 <ucy77777@gmail.com>
2023-01-26 18:57:44 +0900

    Itemモデルの単体テストコード実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -28,7 +28,7 @@ class Item < ApplicationRecord
   validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :product_condition_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :shipping_charge_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
   validates :delivery_day_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
                     format: { with: /\A[0-9]+\z/ }
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,116 @@
 require 'rails_helper'
-
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '商品出品登録' do
+    context '新規登録がうまくいくとき' do
+      it 'product_nameとprice、category_idとproduct_description、product_condition_id、shipping_charge_id、prefecture_id、delivery_day_idが存在すれば登録できる' do
+        expect(@item).to be_valid
+      end
+      it 'category_idが「---」以外であれば登録できる' do
+        @item.category_id = 2
+        expect(@item).to be_valid
+      end
+      it 'product_condition_idが「---」以外であれば登録できる' do
+        @item.product_condition_id = 2
+        expect(@item).to be_valid
+      end
+      it 'shipping_charge_idが「---」以外であれば登録できる' do
+        @item.shipping_charge_id = 2
+        expect(@item).to be_valid
+      end
+      it 'prefecture_idが「---」以外であれば登録できる' do
+        @item.prefecture_id = 2
+        expect(@item).to be_valid
+      end
+      it 'delivery_day_idが「---」以外であれば登録できる' do
+        @item.delivery_day_id = 2
+        expect(@item).to be_valid
+      end
+    end
+    context '新規登録ができないとき' do
+      it 'imageが空では出品できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+      it 'product_nameが空では出品できない' do
+        @item.product_name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Product name can't be blank")
+      end
+      it 'product_descriptionが空では出品できない' do
+        @item.product_description = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Product description can't be blank")
+      end
+      it 'category_idが空では出品できない' do
+        @item.category_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
+      it 'catedory_idが「---」だと出品できない' do
+        @item.category_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
+      it 'product_condition_idが空では出品できない' do
+        @item.product_condition_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Product condition can't be blank")
+      end
+      it 'product_condition_idが「---」だと出品できない' do
+        @item.product_condition_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Product condition can't be blank")
+      end
+      it 'shipping_charge_idが空では出品できない' do
+        @item.shipping_charge_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping charge can't be blank")
+      end
+      it 'shipping_charge_idが「---」だと出品できない' do
+        @item.shipping_charge_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping charge can't be blank")
+      end
+      it 'prefecture_idが空では出品できない' do
+        @item.prefecture_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture can't be blank")
+      end
+      it 'prefecture_idが「---」だと出品できない' do
+        @item.prefecture_id = 0
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture can't be blank")
+      end
+      it 'delivery_day_idが空では出品できない' do
+        @item.delivery_day_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Delivery day can't be blank")
+      end
+      it 'delivery_day_idが「---」だと出品できない' do
+        @item.delivery_day_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Delivery day can't be blank")
+      end
+      it 'priceが空では出品できない' do
+        @item.price = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+      it 'priceが、300円未満だと出品できない' do
+        @item.price = 100
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price must be greater than or equal to 300')
+      end
+      it 'priceが、9,999,999円を超えると出品できない' do
+        @item.price = 10_000_000
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
+      end
+    end
+  end
 end
