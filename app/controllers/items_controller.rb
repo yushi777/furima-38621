f5953640abbb5606b62dcc6dfdfb8b7288a42156
yushi777 <ucy77777@gmail.com>
2023-01-28 20:48:03 +0900

    itemsのindexビューの編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
 
   def index
-     @items = Item.all
+     @items = Item.includes(:user).order('created_at DESC')
   end
 
   def new
