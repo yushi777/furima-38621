a7b6dfa6a0d267738fa0015fa5e63a6734d886b7
yushi777 <ucy77777@gmail.com>
2023-01-30 19:49:07 +0900

    destroyメソッドの作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -35,6 +35,15 @@ class ItemsController < ApplicationController
       redirect_to item_path(item_params)
     else
       render :edit
+    end
+  end
+
+  def destroy
+    if current_user.id == @item.user_id
+    @item.destroy
+    redirect_to root_path
+    else
+      redirect_to root_path
     end
   end
 
