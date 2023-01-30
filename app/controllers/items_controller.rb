9f08265577719b3a14715606ec6108d14c90dd55
yushi777 <ucy77777@gmail.com>
2023-01-30 19:49:35 +0900

    Rubocopの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -40,8 +40,8 @@ class ItemsController < ApplicationController
 
   def destroy
     if current_user.id == @item.user_id
-    @item.destroy
-    redirect_to root_path
+      @item.destroy
+      redirect_to root_path
     else
       redirect_to root_path
     end
