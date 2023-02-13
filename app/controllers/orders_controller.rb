0008635733d2a3de40e1da342922751671d7415a
yushi777 <ucy77777@gmail.com>
2023-02-13 16:15:59 +0900

    決済処理の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,10 +7,10 @@ class OrdersController < ApplicationController
   end
 
   def create
-    
     @order_form = OrderForm.new(order_params)
     @item = Item.find(params[:item_id])
     if @order_form.valid?
+      pay_item
       @order_form.save
       return redirect_to root_path
     else
@@ -20,8 +20,18 @@ class OrdersController < ApplicationController
 
 
   private
+
   def order_params
     params.require(:order_form).permit(:postal_code, :prefecture_id, :municipalities, :address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = "sk_test_a2d562da6a0303d84a941852"  # 自身のPAY.JPテスト秘密鍵を記述しましょう
+      Payjp::Charge.create(
+        amount: @item.price,  # 商品の値段
+        card: order_params[:token],    # カードトークン
+        currency: 'jpy'                 # 通貨の種類（日本円）
+      )
   end
 
  
