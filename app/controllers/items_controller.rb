8d50fcb4a908d7f4ecf6c33c74b1a3aef6696595
yushi777 <ucy77777@gmail.com>
2023-02-13 18:24:59 +0900

    itemsコントローラーの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
-  before_action :move_to_index, only: [:edit, :update, :destroy]
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -24,6 +23,11 @@ class ItemsController < ApplicationController
   end
 
   def edit
+    if
+      @item.user_id == current_user.id && @item.order.nil?
+    else
+     redirect_to root_path
+    end
   end
 
   def update
@@ -36,8 +40,13 @@ class ItemsController < ApplicationController
   end
 
   def destroy
+    if
+    @item.user_id == current_user.id
     @item.destroy
     redirect_to root_path
+  else
+    redirect_to root_path
+  end
   end
 
   private
@@ -51,9 +60,4 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
-  def move_to_index
-    unless @item.user_id == current_user.id
-     redirect_to root_path
-    end
-  end
 end
