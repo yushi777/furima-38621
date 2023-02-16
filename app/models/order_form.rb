4ede027a1a7348d5e8356d87facac47443f0ca43
yushi777 <ucy77777@gmail.com>
2023-02-16 14:20:05 +0900

    order_formモデルの編集

--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -2,21 +2,20 @@ class OrderForm
   include ActiveModel::Model
   attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :municipalities, :address, :building_name, :phone_number, :token
 
-  # ここにバリデーションの処理を書く
-  validates :user_id, presence: true
-  validates :item_id, presence: true
-  validates :postal_code, presence: true, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
+  with_options presence: true do
+  validates :user_id
+  validates :item_id
+  validates :postal_code,  format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
   validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
-  validates :municipalities, presence: true
-  validates :address, presence: true
-  validates :phone_number, presence: true, format: { with: /\A[0-9]{10,11}\z/, message: 'is invalid' }
-  validates :token, presence: true
-  # validates :token, presence: true
+  validates :municipalities
+  validates :address
+  validates :phone_number, format: { with: /\A[0-9]{10,11}\z/, message: 'is invalid' }
+  validates :token
+  end
 
   def save
-    # 各テーブルにデータを保存する処理を書く
     order = Order.create(user_id: user_id, item_id: item_id)
-    Address.create(order_id: order.id, postal_code: postal_code, prefecture_id: prefecture_id, municipalities: municipalities,
+    Address.create(order_id: order.id, postal_code: postal_code, prefecture_id: prefecture_id, municipalities: municipalities, building_name: building_name,
                    address: address, phone_number: phone_number)
   end
 end
